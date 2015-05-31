89da2b196b4ed0bc7e7dd1bc77e68ae73481b683
Chaitanya <ckulkar@gmail.com>
2015-05-31 15:36:37 -0700

    JorSayLabs - a custom CheckBoxPreference class for convenience and completely dynamic preference screen building (no xml)

---
+++ JorSayLabs/app/src/main/java/com/mocha17/slayer/labs/com/mocha17/slayer/labs/backend/CheckBoxPreference.java
@@ -0,0 +1,44 @@
+package com.mocha17.slayer.labs.com.mocha17.slayer.labs.backend;
+
+import android.content.Context;
+
+/**
+ * Created by Chaitanya on 5/31/15.
+ * This is a thin wrapper around the Android CheckBoxPreference, and provides
+ * us with some convenience Constructor and members. Particularly, we have Constructors
+ * that avoid calling multiple methods like setOrder(), setKey() etc. from Fragment code.
+ * Also, the 'added' and 'order' information is stored in this class, and client code can either
+ * directly access it ('added') or doesn't need to worry about it ('order').
+ */
+public class CheckBoxPreference extends android.preference.CheckBoxPreference {
+    private boolean added;
+
+    public CheckBoxPreference(Context context, int order) {
+        super(context);
+        super.setOrder(order);
+    }
+
+    public CheckBoxPreference(Context context, int order, String key, String title) {
+        super(context);
+        super.setOrder(order);
+        super.setKey(key);
+        super.setTitle(title);
+    }
+
+    public CheckBoxPreference(Context context, int order, String key, String title, String summary) {
+        super(context);
+        super.setOrder(order);
+        super.setKey(key);
+        super.setTitle(title);
+        super.setSummary(summary);
+    }
+
+    public boolean isAdded() {
+        return added;
+    }
+
+    public void added(boolean added) {
+        this.added = added;
+    }
+
+}
